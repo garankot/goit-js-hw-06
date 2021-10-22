912a62a25278a2037fa3a085ca6a2ac1585fdad5
Taras Garan'ko <garanko.t@gmail.com>
2021-10-23 02:14:35 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,10 @@
-const totalList = document.querySelector('#categories')
+// Кількість елементів
+const categories = document.querySelector('#categories');
+const allItems = document.querySelectorAll('.item');
+
+function totalCategories (allItems) {
+    console.log(`Number of categories: ${allItems.length}`);
+}
+totalCategories(allItems);
+
+// 
