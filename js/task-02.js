60062c1d774314ab5365fde92d86bedbacbc7110
Taras Garan'ko <garanko.t@gmail.com>
2021-10-24 22:48:48 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listsArr = [];
+
+ingredients.map(ingredient => {
+  const lists = document.createElement("li")
+  lists.textContent = ingredient
+  lists.classList.add('item')
+  listsArr.push(lists)
+})
+document.querySelector('#ingredients').append(...listsArr)
