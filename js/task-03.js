fbad186319e6a77401c44a2d9ae5cc7150f7eb96
Taras Garan'ko <garanko.t@gmail.com>
2021-10-24 23:19:05 +0300

    update

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,15 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+const gallery = document.querySelector(".gallery");
+
+const imgs = images.map(image => 
+  `<li class = "gallery-item">
+  <img class = "gallery-img" src="${image.url}" alt="${image.alt}"/>
+  </li>`
+)
+.join("")
+
+gallery.insertAdjacentHTML('afterbegin', imgs)
