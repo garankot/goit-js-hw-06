9e39c8db406eae0f10ae13fd0d540e44bcbe86bd
Taras Garan'ko <garanko.t@gmail.com>
2021-10-26 22:02:32 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (
+    event.currentTarget.elements.email.value === '' ||
+    event.currentTarget.elements.password.value === ''
+  ) {
+    return alert('All form fields must be filled out!');
+  }
+  const formUserData = {
+    email: event.currentTarget.elements.email.value,
+    password: event.currentTarget.elements.password.value,
+  };
+  console.log(formUserData);
+  event.currentTarget.reset();
+}
