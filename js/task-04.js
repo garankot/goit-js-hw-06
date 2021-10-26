3be60f0e998a4ed7d44f760c1fb12d6f9201b5bd
Taras Garan'ko <garanko.t@gmail.com>
2021-10-26 19:51:08 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+const btnDecrement = document.querySelector('button[data-action="decrement"]');
+const spanValue = document.querySelector('#value');
+const btnIncrement = document.querySelector('button[data-action="increment"]');
+
+let counterValue = 0;
+
+const decrementValue = () => {
+  counterValue -= 1;
+  return (spanValue.textContent = counterValue);
+};
+btnDecrement.addEventListener('click', decrementValue);
+
+const incrementValue = () => {
+  counterValue += 1;
+  return (spanValue.textContent = counterValue);
+};
+
+btnIncrement.addEventListener('click', incrementValue);
