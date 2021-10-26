3d5927fd8741d10598fd3a9c113cb97d154ea0d8
Taras Garan'ko <garanko.t@gmail.com>
2021-10-26 21:17:11 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,6 @@ const rangeInput = document.querySelector('#font-size-control');
 const textSize = document.querySelector('#text');
 
 const rangeChange = () => {
-    textSize.style.fontSize = rangeInput.value + 'px';
+  textSize.style.fontSize = rangeInput.value + 'px';
 };
-rangeInput.addEventListener('input', rangeChange)
+rangeInput.addEventListener('input', rangeChange);
