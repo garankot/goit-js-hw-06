b02dbcbeccea1a7cc7434f608ce87422bab2c727
Taras Garan'ko <garanko.t@gmail.com>
2021-10-26 22:52:37 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const hexColor = document.querySelector('.color');
+const bgSwitcherBtn = document.querySelector('.change-color');
+
+bgSwitcherBtn.addEventListener('click', onChangeColor);
+function onChangeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  hexColor.textContent = getRandomHexColor();
+}
