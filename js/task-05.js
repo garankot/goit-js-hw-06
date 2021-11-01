8536e890c7766ed926dd32f0fb4753f1ae5bd71a
Taras Garan'ko <55506311+garankot@users.noreply.github.com>
2021-11-01 10:38:29 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -5,6 +5,6 @@ const textOutput = () => {
   if (textInput.value === '') {
     return (output.textContent = 'Anonymous');
   }
-  return (output.textContent = textInput.value);
+  output.textContent = textInput.value;
 };
 textInput.addEventListener('input', textOutput);
